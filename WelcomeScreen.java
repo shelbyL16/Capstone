129942f2906fe9471ce121e2e5131c951b265331
Maggie-Fok <31615250+Maggie-Fok@users.noreply.github.com>
2017-09-08 13:52:42 +0200

    Add files via upload

---
+++ WelcomeScreen.java
@@ -0,0 +1,60 @@
+/**
+ * Created by magzf on 2017/09/06.
+ */
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+import javafx.stage.Screen;
+import java.io.*;
+
+public class WelcomeScreen extends Application {
+
+    private static final String MEDIA_URL = "http://download.oracle.com/otndocs/products/javafx/oow2010-2.flv";
+
+    private static final File file = new File("C:/Users/magzf/OneDrive/Documents/2017/CSC3003S/Capstone Project/Welcome screen.mp4");
+    private static final String VIDEO_URL = file.toURI().toString();
+
+    private Button lessonButton;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("SignSupport");
+        Group root = new Group();
+        //Scene scene = new Scene(root, 540, 241);
+        Scene scene = new Scene(root, 600, 500);
+
+
+        // create media player
+       //Media media = new Media(MEDIA_URL);
+        Media media = new Media(VIDEO_URL);
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+
+        VideoPlayer mediaControl = new VideoPlayer(mediaPlayer);
+        scene.setRoot(mediaControl);
+
+        // Add a lesson button - cannot put multiple panes on one stage
+        lessonButton = new Button("Lessons");
+        lessonButton.setLayoutX(100);
+        lessonButton.setLayoutY(300);
+        Pane layout = new Pane();
+        layout.getChildren().add(lessonButton);
+
+        primaryStage.setScene(scene);
+
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        // Set primary stage boundaries to visible bounds of the main screen
+        primaryStage.setX(primaryScreenBounds.getMaxX() - 600);
+        primaryStage.setY(primaryScreenBounds.getMinY());
+
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+}
